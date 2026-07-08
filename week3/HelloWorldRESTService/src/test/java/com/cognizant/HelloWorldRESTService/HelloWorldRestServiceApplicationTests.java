d4879c794ac2596aa01230d5383910e005ceec9b
Chidambaram03 <umschidambaram@gmail.com>
2026-07-08 23:17:07 +0530

    WeeK 3:HelloWorldRESTService, JWT_AuthenticationService, REST_GetCountryByCountryCode, Spring REST using Spring Boot, spring-learn, SpringCore_LoadCountryXML

---
+++ week3/HelloWorldRESTService/src/test/java/com/cognizant/HelloWorldRESTService/HelloWorldRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.HelloWorldRESTService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ week3/JWT_AuthenticationService/src/test/java/com/cognizant/JWT_AuthenticationService/JwtAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.JWT_AuthenticationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
